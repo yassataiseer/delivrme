6ff39df5ecb6f300793f5cc5902b784f626df3be
yassataiseer <1taiseeryas@hdsb.ca>
2021-01-09 17:21:32 -0500

    commit on main

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 import mysql.connector
 
+db =mysql.connector.connect(
+    host = "localhost",
+    user = "root" ,
+    passwd = "root",
+    database = "deliverdatabase"
+)
+#mycursor = db.cursor()
+#mycursor.execute("CREATE TABLE User(Username VARCHAR(50), Password VARCHAR (10), personID int PRIMARY KEY AUTO_INCREMENT)")
+#db.commit()
