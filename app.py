3cdd6c61d61fb43e5b99e1e8da9318e9fd5c049d
yassataiseer <1taiseeryas@hdsb.ca>
2021-01-09 17:26:17 -0500

    git add .

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import mysql.connector
+from flask import Flask, render_template,request,session
+import os
 
 db =mysql.connector.connect(
     host = "localhost",
@@ -6,6 +8,15 @@ db =mysql.connector.connect(
     passwd = "root",
     database = "deliverdatabase"
 )
+
+app = Flask(__name__)
+
 #mycursor = db.cursor()
 #mycursor.execute("CREATE TABLE User(Username VARCHAR(50), Password VARCHAR (10), personID int PRIMARY KEY AUTO_INCREMENT)")
 #db.commit()
+@app.route("/")
+def index():
+    return "hello"
+
+if __name__ == '__main__':
+    app.run(port=50000, debug=True) 
