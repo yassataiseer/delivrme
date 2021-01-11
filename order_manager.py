66d5f444c3c66a85dc36a95cc1409389bd0763ea
yassataiseer <1taiseeryas@hdsb.ca>
2021-01-10 20:06:58 -0500

    commit on main

--- order_manager.py
+++ order_manager.py
@@ -13,15 +13,17 @@ mycursor = db.cursor()
 
 
 class order:
-    def delete_order():
-        pass
+    def delete_order(Username,Address,Item,Price,User_Info):
+        mycursor.execute("DELETE FROM deliveries WHERE Username = %s AND Address = %s AND Item = %s AND Price = %s AND User_Info = %s",(Username,Address,Item,Price,User_Info))
+        db.commit()
+        return True
     def add_order(Username,Address,Item,Price,User_Info):
         mycursor.execute('SELECT * FROM deliveries')
         data = mycursor.fetchall()
         mycursor.execute("INSERT INTO deliveries (Username,Address,Item,Price,User_Info) VALUES (%s,%s,%s,%s,%s)",(Username,Address,Item,Price,User_Info))
         db.commit()
         return True
-    def edit_order():
+    def edit_order(Username,Address,Item,Price,User_Info):
         pass
 
-print(order.add_order("Yassa Taiseer","1328 Whitney Terrace","Box",13,"I need this box delivered ASAP"))
+print(order.delete_order("Yassa Taiseer","1328 Whitney Terrace","Box",13,"I need this box delivered ASAP"))
