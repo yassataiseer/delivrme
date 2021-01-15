91019eaa20f65177895bb417d0b105cacd3638ea
yassataiseer <1taiseeryas@hdsb.ca>
2021-01-15 18:17:51 -0500

    commit on main

--- order_manager.py
+++ order_manager.py
@@ -19,19 +19,31 @@ class order:
         db.commit()
         return True
     def add_order(Username,Address,Item,Price,User_Info):
+        lat = ""
+        lon = ""
+        url = 'http://photon.komoot.de/api/?q='
         mycursor.execute('SELECT * FROM deliveries')
         data = mycursor.fetchall()
-        mycursor.execute("INSERT INTO deliveries (Username,Address,Item,Price,User_Info) VALUES (%s,%s,%s,%s,%s)",(Username,Address,Item,Price,User_Info))
+        resp = requests.get(url=url+Address)
+        data = json.loads(resp.text)
+        a = data['features'][0]['geometry']['coordinates']
+        lat = a[-1]
+        lon = a[0]
+        mycursor.execute("INSERT INTO deliveries (Username,Address,latitude,longitude,Item,Price,User_Info) VALUES (%s,%s,%s,%s,%s,%s,%s)",(Username,Address,lat,lon,Item,Price,User_Info))
         db.commit()
         return True
     def edit_order(Username,Address,Item,Price,User_Info):
         pass
     def get_order():
         mycursor.execute('SELECT * FROM deliveries')
+        data = []
         a = mycursor.fetchall()
-        return list(a)
+        for i in a:
+            i = list(i)
+            data.append(i)
+        return data
 
 
-print(order.get_order())
+#print(order.get_order())
 
 #print(order.add_order("Yassa Taiseer","1328 Whitney Terrace","Box",13,"I need this box delivered ASAP"))
